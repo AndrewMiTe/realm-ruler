9e08c38bede8a2e6efcfa19cc4f489ace8a05870
Andrew Michael Teller <andrew.m.teller@gmail.com>
2016-10-29 23:27:59 -0400

    Fixed package

--- src/client/Client.java
+++ src/client/Client.java
@@ -21,7 +21,8 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package RealmRuler;
+
+package client;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
