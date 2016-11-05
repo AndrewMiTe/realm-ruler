864e7be9592251b1584cf131b9f8d8f8fbd71412
Andrew Michael Teller <andrew.m.teller@gmail.com>
2016-11-05 15:53:40 -0400

    Creates TextRequestView.java

---
+++ src/client/TextRequestView.java
@@ -0,0 +1,74 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Andrew Michael Teller(https://github.com/AndrewMiTe)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package client;
+
+import static client.Client.PADDING_WIDTH;
+import static client.Client.STAGE_HEIGHT;
+import static client.Client.STAGE_WIDTH;
+import java.util.function.Consumer;
+import javafx.geometry.Insets;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+/**
+ *
+ * @author Andrew M. Teller(https://github.com/AndrewMiTe)
+ */
+public class TextRequestView extends View {
+  
+  public TextRequestView(Consumer<String> input, String... messages) {
+    // Adds text to the top.
+    VBox top = new VBox();
+    for (String msg : messages) {
+      Text text = new Text(msg);
+      text.setFont(Font.font("Monospace"));
+      text.setFill(Color.WHITESMOKE);
+      text.setWrappingWidth(STAGE_WIDTH - (2 * PADDING_WIDTH));
+      top.getChildren().add(text);
+    }
+    // Adds text field to the bottom.
+    AnchorPane bottom = new AnchorPane();
+    TextField textInput = new TextField();
+    textInput.setOnAction((e) -> input.accept(textInput.getText()));
+    bottom.getChildren().add(textInput);
+    textInput.setPrefWidth(STAGE_WIDTH - (2 * PADDING_WIDTH));
+    // Creates outer border pane.
+    BorderPane outer = new BorderPane();
+    outer.setPrefHeight(STAGE_HEIGHT);
+    outer.setPadding(
+        new Insets(PADDING_WIDTH, PADDING_WIDTH, PADDING_WIDTH, PADDING_WIDTH)
+    );
+    outer.setTop(top);
+    outer.setBottom(bottom);
+    // Finalizes view.
+    getChildren().add(outer);
+  }
+  
+}
